1da5afa7ff93ad94952f526d10eb3a5f2f599d6d
jerrychen1990 <jerrychen1990@gmail.com>
2021-11-17 11:25:24 +0800

    add __init__

--- snippets/__init__.py
+++ snippets/__init__.py
@@ -10,5 +10,8 @@
 """
 import logging
 
+from snippets.utils import *
+from snippets.decorators import *
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s][%(filename)s:%(lineno)d]:%(message)s",
                     datefmt='%Y-%m-%d %H:%M:%S')
